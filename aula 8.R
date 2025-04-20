ecb2206652d58f1ca57d5f913a4da5bc189ea40a
gmkrug <gmkruger@furb.br>
2025-04-20 12:40:21 -0300

    Add files via upload

---
+++ aula 8.R
@@ -0,0 +1,17 @@
+# avaliação 
+# criar um documento html em quarto
+# isto é, uma pagina de internet com seus gráficos
+# 1 passo
+# instalar o pacote quarto 
+# install.packages('quarto')
+# install.packages("palmerpenguins")
+library(tidyverse)
+library(quarto)
+library(palmerpenguins)
+# 2 passo 
+# criar documento ('RENDER)
+
+# editar o arquivo
+# aleterar title para seu nome
+# modificar a parte (r chunk) das bibiliotecas
+# adicionar o carregamento dos dados 
